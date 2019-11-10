7d936edce2c35fbf9973674333d2cdf20aae2274
volpatto <volpatto@lncc.br>
2019-11-10 02:18:23 -0300

    Try to use conda instead virtualenv

---
+++ .github/workflows/scripts/conda_install_osx.sh
@@ -0,0 +1,3 @@
+wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+rm -rf $HOME/miniconda
+bash miniconda.sh -b -p $HOME/miniconda
---
+++ .github/workflows/scripts/conda_setup.sh
@@ -0,0 +1,9 @@
+bash $HOME/miniconda/etc/profile.d/conda.sh
+export PATH=$HOME/miniconda/bin/:$PATH
+conda config --set always_yes yes --set changeps1 no
+conda config --add channels conda-forge
+conda install conda-devenv
+conda update -q conda
+conda info -a
+conda devenv
+source activate autostep
